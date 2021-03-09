1f95d44f441f0bdaebbd422ccc8338465a16cb3d
dayabot <foo@gmail.com>
2021-03-10 00:35:34 +0800

    fix: 全局异常处理

--- telegram_bot/telegram/__init__.py
+++ telegram_bot/telegram/__init__.py
@@ -1,5 +1,5 @@
 """ manually import to be reachable """
-from telegram_bot.telegram import callbacks, finite_state_machine
+from telegram_bot.telegram import callbacks, finite_state_machine, exception
 
 __all__ = [
     "bot",
---
+++ telegram_bot/telegram/exception.py
@@ -0,0 +1,18 @@
+from aiogram import types
+from aiogram.utils.exceptions import BotBlocked
+from loguru import logger
+from sentry_sdk import capture_exception
+
+from telegram_bot.telegram.dispatcher import dp
+
+
+@dp.errors_handler(exception=BotBlocked)
+async def global_error_handler(update: types.Update, e):
+    logger.warning(e)
+    return True
+
+
+@dp.errors_handler(exception=Exception)
+async def global_error_handler(update: types.Update, e: Exception):
+    capture_exception(e)
+    return True
