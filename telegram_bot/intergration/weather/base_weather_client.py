e5a8eb6d0968ddb386821acd4ad9765b9f062a69
daya0576 <daya0576@gmail.com>
2021-03-21 13:03:06 +0800

    fix: 更新 aiocache 的缓存 key

--- telegram_bot/intergration/weather/base_weather_client.py
+++ telegram_bot/intergration/weather/base_weather_client.py
@@ -9,3 +9,8 @@ class WeatherClient(ABC):
     @abstractmethod
     def get_weather_photo(self, location) -> str:
         raise NotImplementedError
+
+    def __str__(self) -> str:
+        return self.__class__.__name__
+
+    __repr__ = __str__
