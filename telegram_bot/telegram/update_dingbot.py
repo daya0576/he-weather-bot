2765fcf1480ed1e435e766fbda38da52c69a94cb
daya0576 <daya0576@gmail.com>
2021-11-30 02:15:05 +0800

    feat: update ding talk table structure

--- telegram_bot/telegram/update_dingbot.py
+++ telegram_bot/telegram/update_dingbot.py
@@ -2,6 +2,7 @@ import re
 
 from aiogram import types
 from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters import Text
 from aiogram.dispatcher.filters.state import StatesGroup, State
 from aiogram.types import ContentType
 
@@ -25,20 +26,34 @@ class Form(StatesGroup):
     ding_token = State()
 
 
+@dp.message_handler(state='*', commands='cancel')
+@dp.message_handler(Text(equals='cancel', ignore_case=True), state='*')
+async def cancel_handler(message: types.Message, state: FSMContext):
+    """
+    Allow user to cancel any action
+    """
+    current_state = await state.get_state()
+    if current_state is None:
+        return
+
+    await state.finish()
+
+
 @dp.message_handler(commands='ding_token')
 @registered
 async def update_ding_token(message: types.Message):
     await Form.ding_token.set()
-    await TelegramMessageService.send_text(dp.bot, message.chat.id, "Hi！请输入机器人的Webhook地址")
+    await TelegramMessageService.send_text(dp.bot, message.chat.id, "Hi！请回复自定义机器人Webhook地址")
 
 
 @dp.message_handler(state=Form.ding_token, content_types=ContentType.TEXT)
+@dp.message_handler(state=Form.ding_token, content_types=ContentType.ANY)
 async def process_ding_token(message: types.Message, state: FSMContext):
     ding_token = await extra_ding_token_from_message(message.text)
     if not ding_token:
-        await message.reply("机器人的Webhook地址非法，参考：https://oapi.dingtalk.com/robot/send?access_token=XXXXXX:")
-        await state.finish()
-        return
+        return await message.reply("机器人的Webhook地址非法!"
+                                   "\n参考格式：https://oapi.dingtalk.com/robot/send?access_token=XXXXXX"
+                                   "\n取消输入：/cancel")
 
     with get_db_session() as db:
         crud.update_or_create_ding_bot(db, message.chat.id, ding_token)
