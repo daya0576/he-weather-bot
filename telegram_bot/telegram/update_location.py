8101356b2acb1cf1797402216914c1f1dc0413de
大牙(Henry Zhu) <daya0576@users.noreply.github.com>
2021-12-06 16:58:12 +0800

    Update update_location.py

--- telegram_bot/telegram/update_location.py
+++ telegram_bot/telegram/update_location.py
@@ -30,7 +30,7 @@ class Form(StatesGroup):
     location = State()
 
 
-@dp.message_handler(commands='update_location')
+@dp.message_handler(commands='set_location')
 async def update_location(message: types.Message):
     await Form.location.set()
     await TelegramMessageService.send_text(dp.bot, message.chat.id, "请回复当前城市关键字，或者模糊定位。  /cancel")
