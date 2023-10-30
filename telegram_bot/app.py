23b92f82c5815758baebb63a37650b2ba9a8e0be
daya0576 <daya0576@gmail.com>
2023-10-30 21:28:57 +0800

    fix: resolve dependency issue

--- telegram_bot/app.py
+++ telegram_bot/app.py
@@ -38,13 +38,15 @@ async def startup_event():
 
 # sentry middleware
 if settings.SENTRY_URL:
+    logger.info("start sentry...")
     sentry_sdk.init(
         dsn=settings.SENTRY_URL,
-        environment=settings.ENV,
-        # To set a uniform sample rate
+        # Set traces_sample_rate to 1.0 to capture 100%
+        # of transactions for performance monitoring.
+        traces_sample_rate=1.0,
         # Set profiles_sample_rate to 1.0 to profile 100%
         # of sampled transactions.
-        # We recommend adjusting this value in production
+        # We recommend adjusting this value in production.
         profiles_sample_rate=1.0,
     )
     app = SentryAsgiMiddleware(app)
